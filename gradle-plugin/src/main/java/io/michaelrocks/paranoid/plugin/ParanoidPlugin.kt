2bc24219c73c176db274248e9a3964e94794febb
Adam Kvasnička <adam.kvasnicka@codegrowers.tech>
2025-04-17 08:21:23 +0200

    update plugin

--- gradle-plugin/src/main/java/io/michaelrocks/paranoid/plugin/ParanoidPlugin.kt
+++ gradle-plugin/src/main/java/io/michaelrocks/paranoid/plugin/ParanoidPlugin.kt
@@ -17,25 +17,46 @@
 package io.michaelrocks.paranoid.plugin
 
 import com.android.build.gradle.BaseExtension
+import com.android.build.api.instrumentation.ClassContext
+import com.android.build.api.instrumentation.ClassData
+import com.android.build.api.instrumentation.InstrumentationParameters
+import com.android.build.api.instrumentation.AsmClassVisitorFactory
+import com.android.build.api.variant.AndroidComponentsExtension
+import com.android.build.api.instrumentation.InstrumentationScope
+import com.android.build.gradle.AppPlugin
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.UnknownDomainObjectException
 import org.gradle.api.plugins.JavaPlugin
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.Opcodes
 
 class ParanoidPlugin : Plugin<Project> {
   private lateinit var extension: ParanoidExtension
 
   override fun apply(project: Project) {
-    extension = project.extensions.create("paranoid", ParanoidExtension::class.java)
+     extension = project.extensions.create("paranoid", ParanoidExtension::class.java)
 
-    try {
-      val android = project.extensions.getByName("android") as BaseExtension
-      project.addDependencies(getDefaultConfiguration())
-      android.registerTransform(ParanoidTransform(extension, android))
-    } catch (exception: UnknownDomainObjectException) {
-      throw GradleException("Paranoid plugin must be applied *AFTER* Android plugin", exception)
-    }
+        try {
+            // Add dependency
+            project.addDependencies(getDefaultConfiguration())
+
+            // Ensure Android plugin is applied
+            project.plugins.withType(AppPlugin::class.java) {
+                val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
+                androidComponents.onVariants { variant ->
+                    variant.instrumentation.transformClassesWith(
+                        ParanoidVisitorFactory::class.java,
+                        InstrumentationScope.ALL
+                    ) {
+                        // Optional: pass parameters if needed
+                    }
+                }
+            }
+        } catch (exception: UnknownDomainObjectException) {
+            throw GradleException("Paranoid plugin must be applied *AFTER* Android plugin", exception)
+        }
   }
 
   private fun getDefaultConfiguration(): String {
@@ -46,4 +67,25 @@ class ParanoidPlugin : Plugin<Project> {
     val version = Build.VERSION
     dependencies.add(configurationName, "io.michaelrocks:paranoid-core:$version")
   }
+}
+
+abstract class ParanoidVisitorFactory :
+    AsmClassVisitorFactory<InstrumentationParameters.None> {
+
+    override fun isInstrumentable(classData: ClassData): Boolean {
+        // Customize logic to filter which classes to instrument
+        return true
+    }
+
+    override fun createClassVisitor(
+        classContext: ClassContext,
+        nextClassVisitor: ClassVisitor,
+        parameters: InstrumentationParameters.None
+    ): ClassVisitor {
+        return ParanoidClassVisitor(nextClassVisitor)
+    }
+}
+
+class ParanoidClassVisitor(cv: ClassVisitor) : ClassVisitor(Opcodes.ASM9, cv) {
+    // Modify class/methods here
 }
